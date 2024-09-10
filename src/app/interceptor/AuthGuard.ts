46868d298a9215dc363852fbdebdae4b7bc91cf3
gotosleep2409 <vanh24092002@gmail.com>
2024-09-10 12:25:51 +0700

    Update: Fix Router Navigation Issue

--- src/app/interceptor/AuthGuard.ts
+++ src/app/interceptor/AuthGuard.ts
@@ -27,7 +27,7 @@ export class AuthGuard implements CanActivate {
         return false;
       }
     } else {
-      this.router.navigate(['/login']);
+      this.router.navigate(['/home']);
       return false;
     }
   }
